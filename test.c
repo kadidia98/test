1176f4dd82202985cef87568edb9767f0136ab45
kadidia <diallokhadidiatoubouba@gmail.com>
2022-08-22 17:22:01 +0000

    verification

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+int a;
+printf("nombre");
+scanf("%d",&a);
+if (a>0)
+{
+    printf("le nombre est positif");
+}
+else{
+    printf("le nombre est negatif");
+}
+
+
+
+
+
+
+
+return 0;
+}
